9f43b4842b169b406ca5aafcb0d9d4bee1646115
mredmundto <yatlunto@gmail.com>
2016-06-08 16:17:33 -0700

    Make all stack prompts pass

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance.storage = {};
   someInstance.key = 0; 
 
   _.extend(someInstance, stackMethods);
@@ -15,3 +16,13 @@ stackMethods.size = function() {
   return this.key; 
 };
 
+stackMethods.push = function(input) {
+  this.storage[this.key] = input; 
+  this.key++; 
+};
+
+stackMethods.pop = function() {
+  this.key--;
+  if (this.key < 0) { this.key = 0; }
+  return this.storage[ this.key ];
+};
