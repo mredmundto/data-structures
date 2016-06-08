0e21c7f73ae0f23dca168f3872ae6b8071f0b90a
mredmundto <yatlunto@gmail.com>
2016-06-08 15:47:20 -0700

    Make all code for queue pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var key = 0; 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[0];
+    for (var keys in storage) {
+      if (storage[Number(keys) + 1] !== undefined) {
+        storage[keys] = storage[Number(keys) + 1];  
+      } else {
+        delete storage[keys];
+      }
+    } 
+    key--; 
+    return temp;
   };
 
   someInstance.size = function() {
+    if (key < 0) { return 0; } 
+    return key; 
   };
 
   return someInstance;
