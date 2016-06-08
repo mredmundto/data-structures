c2d5f178d88edd84c6d83f1e4b787470b7b53d58
mredmundto <yatlunto@gmail.com>
2016-06-08 16:34:47 -0700

    Increase size when items added

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.storage = {}; 
+  someInstance.key = 0;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return this.key;
+};
 
+queueMethods.enqueue = function(input){
+  this.storage[this.key] = input;
+  this.key++; 
+};
