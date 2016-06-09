46dae3fbe84367cf934fa81cb5e1955900b66638
mredmundto <yatlunto@gmail.com>
2016-06-08 20:06:55 -0700

    Completed queue in functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -15,7 +15,27 @@ queueMethods.size = function() {
   return this.key;
 };
 
-queueMethods.enqueue = function(input){
+queueMethods.enqueue = function(input) {
   this.storage[this.key] = input;
   this.key++; 
+};
+
+queueMethods.dequeue = function() {
+  // store the first value in the quene, to return later 
+  var temp = this.storage[0];
+  
+  for (var keys in this.storage) {
+    // to check if last key in storage
+    if (this.storage[Number(keys) + 1] !== undefined) {
+      // if not last key, move next one in storage to the current key 
+      this.storage[keys] = this.storage[Number(keys) + 1 ];
+    } else {
+      // if last, delete 
+      delete this.storage[keys];
+    }
+  }
+  
+  this.key--; 
+  if (this.key < 0) { this.key = 0; }
+  return temp;
 };
