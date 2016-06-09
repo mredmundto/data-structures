4d1d2a60c724f0c2eb57dc88e455d0e55f692938
mredmundto <yatlunto@gmail.com>
2016-06-08 20:33:58 -0700

    Complete promts for stack in prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstances = Object.create(stackMethods);
+  someInstances.key = 0; 
+  someInstances.storage = {};
+  return someInstances; 
+
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.key; 
+};
 
+stackMethods.push = function(input) {
+  this.storage[this.key] = input; 
+  this.key++;
+
+};
+
+stackMethods.pop = function() {
+  // the item is still in the object, just point the key to previous one
+  this.key--; 
+  if (this.key < 0) { this.key = 0; }
+  // key refers to the last value and returning the value 
+  return this.storage[this.key];
+};
